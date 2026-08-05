41c63ae22b4cea9adc7ca650b03d59bfb40a241e
commacashing <commacashing@gmail.com>
2026-08-04 22:43:29 -0400

    Add custom User-Agent header to RSS parser

--- netlify/functions/feed.js
+++ netlify/functions/feed.js
@@ -1,6 +1,10 @@
 import RssParser from 'rss-parser';
 
-const parser = new RssParser();
+const parser = new RssParser({
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+  }
+});
 const FEED_URL = 'https://www.advisorhub.com/category/advisor-moves/feed/';
 
 export async function handler() {
