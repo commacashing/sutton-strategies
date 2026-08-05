5a426b628fad0d678a8551dba44ae474675ba755
commacashing <commacashing@gmail.com>
2026-08-04 22:45:47 -0400

    Add Accept and Accept-Language headers to RSS parser

--- netlify/functions/feed.js
+++ netlify/functions/feed.js
@@ -2,7 +2,9 @@ import RssParser from 'rss-parser';
 
 const parser = new RssParser({
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    'Accept': 'application/rss+xml, application/xml, text/xml, */*',
+    'Accept-Language': 'en-US,en;q=0.9'
   }
 });
 const FEED_URL = 'https://www.advisorhub.com/category/advisor-moves/feed/';
